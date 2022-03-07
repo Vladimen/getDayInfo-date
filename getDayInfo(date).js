1e10c19f81247decc3e684929a070a1571a5a668
Vladimen <95418051+Vladimen@users.noreply.github.com>
2022-03-08 01:19:02 +0300

    Add files via upload

---
+++ getDayInfo(date).js
@@ -0,0 +1,42 @@
+<script>
+
+getDate('09.12.2022');
+
+const getDate = (dateString) => {
+const day = dateString.substring(0,2);
+const month = Number(dateString.substring(3,5)) - 1;
+const year = dateString.substring(6,10);
+const date = new Date(year, month, day);
+const oneJan = new Date(date.getFullYear(),0,1);
+const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000));
+const result = Math.ceil(( date.getDay() + 1 + numberOfDays) / 7);
+const dayWeek = date.getDay();
+const days = [
+'Воскресенье',
+'Понедельник',
+'Вторник',
+'Среда',
+'Четверг',
+'Пятница',
+'Суббота'
+];
+const months = [
+'января',
+'февраля',
+'марта',
+'апреля',
+'мая',
+'июня',
+'июля',
+'августа',
+'сентября',
+'отктября',
+'ноября',
+'декабря',
+];
+const localDayWeek = days[dayWeek];
+const localMonth = months[month];
+console.log(localDayWeek + ",", result + " неделя", localMonth, year + " года");
+}
+
+</script>
